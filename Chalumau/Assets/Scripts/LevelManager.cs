fa0750c3e9e8cf0a1c81677a688aa7aa779b1d8d
LADY_LAPTOPIA\minig <maximeflageole@gmail.com>
2023-08-02 14:44:50 -0400

    Adding a building placement verification/occupied space for buildings

---
+++ Chalumau/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager _Instance;
+
+    private List<Placeable> m_placeableObjects;
+
+    private void Awake()
+    {
+        _Instance = this;
+        m_placeableObjects = new List<Placeable>();
+    }
+
+    public bool CanPlacePlaceable(Placeable placeable)
+    {
+        foreach (var placedObject in m_placeableObjects)
+        {
+            if (Collides(placedObject, placeable))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool Collides(Placeable placeable1, Placeable placeable2)
+    {
+        foreach (var space in placeable1.OccupiedSpace)
+        {
+            foreach (var space2 in placeable2.OccupiedSpace)
+            {
+                Debug.Log("space1 = " + space + " space2: " + space2);
+                if (space == space2)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void PlacePlaceable(Placeable placeable)
+    {
+        m_placeableObjects.Add(placeable);
+    }
+}
+
+public enum ECardinalDirection
+{
+    North,
+    East,
+    South,
+    West,
+    Count
+}
--- Chalumau/Assets/Scripts/Placeable.cs
+++ Chalumau/Assets/Scripts/Placeable.cs
@@ -1,9 +1,35 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Placeable : MonoBehaviour
 {
-    [SerializeField] float m_clippingUnitValue = 1.0f;
-    bool m_inPlacement = true;
+    [SerializeField] 
+    protected float m_clippingUnitValue = 1.0f;
+    [SerializeField]
+    protected BuildingData m_buildingData;
+    [SerializeField]
+    protected Material m_invalidMaterial;
+    [SerializeField]
+    protected Material m_validMaterial;
+    [SerializeField]
+    protected Material m_placedMaterial;
+
+    [field:SerializeField]
+    public ECardinalDirection Direction { get; protected set; } = ECardinalDirection.North;
+    protected Vector2Int m_cornerPosition;
+    protected bool m_inPlacement = true;
+
+    public List<Vector2Int> OccupiedSpace { get; protected set; } = new List<Vector2Int>();
+
+    private void Awake()
+    {
+        if (m_inPlacement)
+        {
+            GetComponent<Renderer>().material = m_validMaterial;
+            return;
+        }
+        GetComponent<Renderer>().material = m_placedMaterial;
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,12 +43,24 @@ public class Placeable : MonoBehaviour
             }
             UpdateBuildingLocation();
             UpdateBuildingRotation();
+            UpdateOccupiedSpace();
+            if (LevelManager._Instance.CanPlacePlaceable(this))
+            {
+                GetComponent<Renderer>().material = m_validMaterial;
+            }
+            else
+            {
+                GetComponent<Renderer>().material = m_invalidMaterial;
+            }
         }
     }
 
     void PlaceBuilding()
     {
         m_inPlacement = false;
+        OccupiedSpace = new List<Vector2Int>();
+        UpdateOccupiedSpace();
+        LevelManager._Instance.PlacePlaceable(this);
     }
 
     void UpdateBuildingLocation()
@@ -37,6 +75,7 @@ public class Placeable : MonoBehaviour
                hit.point.y,
                Mathf.Floor(hit.point.z));
 
+            m_cornerPosition = new Vector2Int((int)clippedPoint.x, (int)clippedPoint.z);
             transform.position = clippedPoint;
         }
     }
@@ -45,11 +84,89 @@ public class Placeable : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.Rotate(new Vector3(0, 90, 0));
+            transform.Rotate(new Vector3(0, -90, 0));
+            Direction--;
+            if (Direction < 0)
+            {
+                Direction += (int)ECardinalDirection.Count;
+            }
+            Direction = (ECardinalDirection)((int)Direction % (int)(ECardinalDirection.Count));
+            Debug.Log(Direction.ToString());
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.Rotate(new Vector3(0, -90, 0));
+            Direction++;
+            transform.Rotate(new Vector3(0, 90, 0));
+            Direction = (ECardinalDirection)((int)Direction % (int)(ECardinalDirection.Count));
+            Debug.Log(Direction.ToString());
         }
+    }
+
+    protected void UpdateOccupiedSpace()
+    {
+        OccupiedSpace.Clear();
+        Vector2Int currentPos = new Vector2Int(m_cornerPosition.x, m_cornerPosition.y);
+
+        for (int i = 0; i < m_buildingData.m_dimensions.Length; i++)
+        {
+            currentPos.y = m_cornerPosition.y;
+
+            for (int j = 0; j < m_buildingData.m_dimensions[i].Array.Length; j++)
+            {
+                if (m_buildingData.m_dimensions[i].Array[j])
+                {
+                    OccupiedSpace.Add(new Vector2Int(currentPos.x, currentPos.y));
+                }
+                TranslateAccordingToDirection(ref currentPos, false, true);
+            }
+            TranslateAccordingToDirection(ref currentPos, true, false);
+        }
+        GetComponent<Renderer>().material = m_placedMaterial;
+    }
+
+    void TranslateAccordingToDirection(ref Vector2Int vector, bool x, bool y)
+    {
+        Vector2Int translation = new Vector2Int();
+        if (x)
+        {
+            switch (Direction)
+            {
+                case ECardinalDirection.North:
+                    translation += new Vector2Int(1, 0);
+                    break;
+                case ECardinalDirection.East:
+                    translation += new Vector2Int(0, 1);
+                    break;
+                case ECardinalDirection.South:
+                    translation += new Vector2Int(-1, 0);
+                    break;
+                case ECardinalDirection.West:
+                    translation += new Vector2Int(0, -1);
+                    break;
+                case ECardinalDirection.Count:
+                    break;
+            }
+        }
+        if (y)
+        {
+            switch (Direction)
+            {
+                case ECardinalDirection.North:
+                    translation += new Vector2Int(0, 1);
+                    break;
+                case ECardinalDirection.East:
+                    translation += new Vector2Int(1, 0);
+                    break;
+                case ECardinalDirection.South:
+                    translation += new Vector2Int(0, -1);
+                    break;
+                case ECardinalDirection.West:
+                    translation += new Vector2Int(-1, 0);
+                    break;
+                case ECardinalDirection.Count:
+                    break;
+            }
+        }
+        vector += translation;
     }
 }
